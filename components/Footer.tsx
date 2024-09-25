67b660fa06d363e9839d58cdc69c5a660c5aee7b
TeksuSiK <oskar@teksusik.pl>
2024-09-25 16:39:13 +0200

    Empty project

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-right text-xs text-gray-600 dark:text-gray-400 absolute bottom-2 right-4">
-      <p>{new Date().getFullYear()} - Oskar Dyka</p>
-    </footer>
-  );
-};
-
-export default Footer;
