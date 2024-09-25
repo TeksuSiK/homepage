abeeb2aeb5aa4e98b1ec94ea7b6f6be00bd226fb
TeksuSiK <oskar@teksusik.pl>
2024-09-25 17:41:34 +0200

    Add content for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-    return <div></div>
+    return (
+        <div>
+            <div className="mb-8 text-2xl font-semibold">Oskar Dyka</div>
+            <div className="mb-4">
+                <div className="mb-2">
+                    Hi, I’m Oskar from Poland, and I’ve been programming as a
+                    hobby for about five years. For the past few months, I’ve
+                    been working as a QA Engineer at a software house, where I
+                    focus on ensuring the quality of software while continuing
+                    to expand my technical expertise. I’m especially passionate
+                    about backend technologies and server administration, and
+                    I’m always eager to learn more in these areas.
+                </div>
+
+                <div>
+                    Alongside my job, I’m a student at a technical university,
+                    where I’m honing my skills in computer science and software
+                    development. I enjoy taking on new challenges and constantly
+                    improving my abilities.
+                </div>
+            </div>
+        </div>
+    )
 }
