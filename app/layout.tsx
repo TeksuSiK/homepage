43a1c442f25e0aa1979e9d08a3ac0385d23aa4ab
TeksuSiK <oskar@teksusik.pl>
2024-09-25 17:18:08 +0200

    Remove default fonts, create navbar component, add navbar to site layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from './navbar'
 
 export const metadata: Metadata = {
     title: "Oskar 'teksusik.' Dyka - Homepage",
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+                <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+                    <Navbar />
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
