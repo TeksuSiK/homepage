dc3bee5eff74dedf6e9f357a0e893335933f7bf6
TeksuSiK <oskar@teksusik.pl>
2024-09-26 12:36:53 +0200

    Fix bottom margin on 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Custom404() {
     return (
-        <div className="flex flex-col items-center min-h-screen text-center">
+        <div className="flex flex-col items-center text-center mb-4">
             <h1 className="text-5xl font-bold mb-4">404</h1>
             <p className="text-xl mb-8">
                 Oops! The page you’re looking for doesn’t exist.
