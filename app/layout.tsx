0864be8826f8a548d421e3db511451e90492826a
TeksuSiK <oskar@teksusik.pl>
2024-09-26 08:59:42 +0200

    Add theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Providers from './providers'
 import Navbar from './navbar'
 import Footer from './footer'
 
@@ -15,12 +16,17 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-                <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-                    <Navbar />
-                    {children}
-                    <Footer />
-                </main>
+            <body
+                className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto"
+                suppressHydrationWarning
+            >
+                <Providers>
+                    <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+                        <Navbar />
+                        {children}
+                        <Footer />
+                    </main>
+                </Providers>
             </body>
         </html>
     )
